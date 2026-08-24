1a61d84b4e253f1d57d1d84ebf74502d7aa7d74a
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-08-24 22:59:50 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int min=0;
+    int matrix[5][5];
+ 
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cin>>matrix[i][j];
+            if(matrix[i][j]==1)
+            {
+                min = abs(i-2)+abs(j-2);
+            }
+        }
+    }
+    cout<<min<<endl;
+    return 0;
+ 
+}
