9d6411872c493f08558858fd42ab58e590b6e5e6
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-08-24 22:59:12 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        string s;
+        cin>>s;
+        int m = s.length();
+        if(m>10)
+        {
+            cout<<s[0]<<m-2<<s[m-1]<<endl;
+        }
+        else
+        {
+            cout<<s<<endl;
+        }
+    }
+    return 0;
+}
