aa4d553c6bfba27846b756ffb9fa2d27ef148be5
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-08-24 23:01:42 +0530

    Add Blackboard Game [A] from Codeforces

---
+++ 2123/A - Blackboard Game/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t,n;
+    cin>>t;
+ 
+    while(t--)
+    {
+        cin>>n;
+ 
+        if(n % 4 == 0)
+            cout<<"Bob
+";
+        else
+            cout<<"Alice
+";
+    }
+ 
+    return 0;
+}
