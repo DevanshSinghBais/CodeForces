36ea5d75ccc8852f0752b3f0c4d015ce2826f3db
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-08-24 22:56:00 +0530

    Add Kuroni and Impossible Calculation [C] from Codeforces

---
+++ 1305/C - Kuroni and Impossible Calculation/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+		int n, m;
+		cin >> n >> m;
+ 
+		vector<int> a(n);
+		for (auto &i : a) cin >> i;
+ 
+		if (n > m) {
+				cout << 0;
+				return 0;
+		}
+ 
+		int ans = 1;
+ 
+		for (int i = 0; i < n; i++) {
+				for (int j = i + 1; j < n; j++) {
+						ans = (ans * (abs(a[i] - a[j]) % m)) % m;
+				}
+		}
+ 
+		cout << ans;
+		return 0;
+}
