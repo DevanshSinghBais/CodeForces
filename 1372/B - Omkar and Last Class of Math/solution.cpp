a12cf51b116a98f7de7fec0b5e4715f85893cd0f
Devansh Singh Bais <devanshsinghbais111@gmail.com>
2026-08-25 12:10:40 +0530

    Add Omkar and Last Class of Math [B] from Codeforces

---
+++ 1372/B - Omkar and Last Class of Math/solution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+ 
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+ 
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        ll a, b;
+        long long p = n - 1;
+ 
+        for (long long i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                p = i - 1;
+                break;
+            }
+        }
+ 
+        p++;
+        a = n/p;
+        b = n - a;
+        cout<<a<<" "<<b<<endl;
+    }
+ 
+    return 0;
+}
