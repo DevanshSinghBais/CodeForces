8b3ca6ebbda1a7857863bbf771d66f8968fb953d
Devansh Singh Bais <devanshsinghbais111@gmail.com>
2026-08-25 12:07:54 +0530

    Add Don't Try to Count [A] from Codeforces

---
+++ 1881/A - Don't Try to Count/solution.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, m;
+        cin >> n >> m;
+ 
+        string x, s;
+        cin >> x >> s;
+ 
+        int moves = 0;
+ 
+        
+        while(x.length() < s.length())
+        {
+            x += x;
+            moves++;
+        }
+ 
+        
+        if(x.find(s) != string::npos)
+        {
+            cout << moves << endl;
+            continue;
+        }
+ 
+        
+        x += x;
+        moves++;
+ 
+        if(x.find(s) != string::npos)
+        {
+            cout << moves << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+    }
+}
