04f400c4adaa6c6ba0b2a7c4f869c259f7f80b0f
Devansh Singh Bais <devanshsinghbais111@gmail.com>
2026-08-25 12:08:52 +0530

    Add M-arrays [B] from Codeforces

---
+++ 1497/B - M-arrays/solution.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+ 
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+ 
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+ 
+        vector<ll> cnt(m, 0);
+ 
+        for (int i = 0; i < n; i++)
+        {
+            ll x;
+            cin >> x;
+            cnt[x % m]++;
+        }
+ 
+        ll ans = 0;
+ 
+        if (cnt[0])
+            ans++;
+ 
+        for (ll i = 1; i <= (m - 1) / 2; i++)
+        {
+            ll a = cnt[i];
+            ll b = cnt[m - i];
+ 
+            if (a == 0 && b == 0)
+                continue;
+ 
+            ans++;
+ 
+            ll mn = min(a, b);
+            ll mx = max(a, b);
+ 
+            if (mx > mn + 1)
+                ans += mx - mn - 1;
+        }
+ 
+        if (m % 2 == 0 && cnt[m / 2])
+            ans++;
+ 
+        cout << ans << endl;
+    }
+ 
+    return 0;
+}
