4cab0c009370557d6850594cc231e222839ba659
Devansh Singh Bais <devanshsinghbais111@gmail.com>
2026-08-25 12:06:53 +0530

    Add Flip Flops [A] from Codeforces

---
+++ 2209/A - Flip Flops/solution.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        long long n, c, k;
+        cin >> n >> c >> k;
+ 
+        vector<long long> a(n);
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+ 
+        sort(a.begin(), a.end());
+ 
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] > c) break;
+ 
+            long long use = min(k, c - a[i]);
+ 
+            c += a[i] + use;
+            k -= use;
+        }
+ 
+        cout << c << endl;
+    }
+}
